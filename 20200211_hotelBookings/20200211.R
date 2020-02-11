da032b5e136a147fddb038f15670f323f31c2045
Kelvin Gorospe <kdgorospe@users.noreply.github.com>
2020-02-11 18:58:51 -0500

    Create 20200211.R
    load data

---
+++ 20200211_hotelBookings/20200211.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(tsibble)
+
+# Load data
+hotels <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-11/hotels.csv')
